a10a5fe68d98c29a7cad75a12ea7bd01925a608e
Raul Henrik Del Pozo <raulinhenke@gmail.com>
2026-05-17 22:10:46 +0200

    klar

--- src/script.js
+++ src/script.js
@@ -1,27 +1,38 @@
 // Definera array av namn
-let people;
+let people = ["Alice", "Bob", "Charlie", "David", "Eve"];
 
 
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
-  
+  const searchInput = document.getElementById("searchInput");
+  const searchBtn = document.getElementById("searchBtn");
+  const resultDisplay = document.getElementById("resultDisplay");
   // Lägg till en klickhändelse på "Sök"-knappen
   searchBtn.addEventListener("click",() => {
   
     // 1. Hämta texten i searchInput.value
+    let name = searchInput.value
     
     // 2. Skapa en boolean för found
-    
+    let found = false
     // 3. Loopa igenom people-arrayen
-    
+    for (let person of people) {
+
     // 4. Jämför texten med varje namn i arrayen
-    
+     if (person === name) {
+
     // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
-    
+    resultDisplay.innerHTML = "Namn hittades!"
+        found = true
+        
     // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-    
-    });
+    } else {
+        resultDisplay.innerHTML = "Namn hittades inte."
+        found = false
+    }
+    }
+  });
 }
 
 
